4d8ffab6fcc8c381d753a42f76c4d16221799b89
dingyingsi <dingyingsi@126.com>
2020-06-04 11:29:21 +0800

    doc

--- datasource-spring-boot-autoconfigure/src/main/java/com/dys/spirngcloud/datasource/properties/Third.java
+++ datasource-spring-boot-autoconfigure/src/main/java/com/dys/spirngcloud/datasource/properties/Third.java
@@ -15,6 +15,92 @@ public class Third {
     private Boolean removeAbandonedOnMaintenance;
     private Boolean removeAbandonedOnBorrow;
     private Boolean jdbcCompliantTruncation;
+    public String getUrl() {
+        return url;
+    }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getInitialSize() {
+        return initialSize;
+    }
+
+    public void setInitialSize(Integer initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public Integer getMaxTotal() {
+        return maxTotal;
+    }
+
+    public void setMaxTotal(Integer maxTotal) {
+        this.maxTotal = maxTotal;
+    }
+
+    public Integer getMaxIdle() {
+        return maxIdle;
+    }
+
+    public void setMaxIdle(Integer maxIdle) {
+        this.maxIdle = maxIdle;
+    }
+
+    public Integer getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(Integer minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public Long getMaxWaitMillis() {
+        return maxWaitMillis;
+    }
+
+    public void setMaxWaitMillis(Long maxWaitMillis) {
+        this.maxWaitMillis = maxWaitMillis;
+    }
+
+    public Boolean getRemoveAbandonedOnMaintenance() {
+        return removeAbandonedOnMaintenance;
+    }
+
+    public void setRemoveAbandonedOnMaintenance(Boolean removeAbandonedOnMaintenance) {
+        this.removeAbandonedOnMaintenance = removeAbandonedOnMaintenance;
+    }
+
+    public Boolean getRemoveAbandonedOnBorrow() {
+        return removeAbandonedOnBorrow;
+    }
+
+    public void setRemoveAbandonedOnBorrow(Boolean removeAbandonedOnBorrow) {
+        this.removeAbandonedOnBorrow = removeAbandonedOnBorrow;
+    }
+
+    public Boolean getJdbcCompliantTruncation() {
+        return jdbcCompliantTruncation;
+    }
+
+    public void setJdbcCompliantTruncation(Boolean jdbcCompliantTruncation) {
+        this.jdbcCompliantTruncation = jdbcCompliantTruncation;
+    }
 
 }
