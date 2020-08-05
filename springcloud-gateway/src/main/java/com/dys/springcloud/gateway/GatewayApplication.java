48b28376c60ffd3425063008ecc85d21b9e74e4b
dingyingsi <dingyingsi@126.com>
2020-08-05 18:31:15 +0800

    doc

--- springcloud-gateway/src/main/java/com/dys/springcloud/gateway/GatewayApplication.java
+++ springcloud-gateway/src/main/java/com/dys/springcloud/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.dys.springcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -13,4 +15,13 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocator routeLocator = routeLocatorBuilder
+                .routes()
+                .route(r -> r.path("/sina")
+                .uri("http://sina.com.cn:80")
+                .id("sina_route")).build();
+        return routeLocator;
+    }
 }
