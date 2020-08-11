619b8206df678eccf7278600e3373ac08a92989a
dingyingsi <dingyingsi@126.com>
2020-08-11 17:06:16 +0800

    doc

--- springcloud-product/src/main/java/com/dys/springcloud/product/ProductionApplication.java
+++ springcloud-product/src/main/java/com/dys/springcloud/product/ProductionApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = {"com.dys"})
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class ProductionApplication {
 
     public static void main(String[] args) {
